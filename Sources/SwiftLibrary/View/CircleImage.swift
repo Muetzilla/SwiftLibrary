69e1e7f6a92b7ab0491ae302e344831f89703e9c
Muetzilla <moritz.gasbichler@ubs.com>
2023-01-05 09:57:09 +0100

    Also take line width as variable

--- Sources/SwiftLibrary/View/CircleImage.swift
+++ Sources/SwiftLibrary/View/CircleImage.swift
@@ -3,15 +3,15 @@ import SwiftUI
 @available(macOS 10.15, *)
 public struct CircleImage: View {
     var image: Image
-    var lineWidth: Int
+    var lineWidth: CGFloat
     
     public var body: some View {
         image
             .clipShape(Circle())
-            .overlay(Circle().stroke(.gray, lineWidth: 4))
+            .overlay(Circle().stroke(.gray, lineWidth: lineWidth))
     }
     
-    public init(imageToSet: Image, lineWidth: Int){
+    public init(imageToSet: Image, lineWidth: CGFloat){
         self.image = imageToSet
         self.lineWidth = lineWidth
     }
